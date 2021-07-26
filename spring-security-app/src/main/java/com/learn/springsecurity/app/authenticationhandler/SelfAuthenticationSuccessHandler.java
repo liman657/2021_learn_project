6dbe550e57f7fe8ac8466abbc6199b064de053c5
liman657 <657271181@qq.com>
2021-07-26 22:35:52 +0800

    【spring-security】第六章，spring-security自定义token登录

--- spring-security-app/src/main/java/com/learn/springsecurity/app/authenticationhandler/SelfAuthenticationSuccessHandler.java
+++ spring-security-app/src/main/java/com/learn/springsecurity/app/authenticationhandler/SelfAuthenticationSuccessHandler.java
@@ -4,8 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.learn.springsecurity.core.properties.LoginType;
 import com.learn.springsecurity.core.properties.SecurityProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.UnapprovedClientAuthenticationException;
+import org.springframework.security.oauth2.provider.*;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -25,9 +34,12 @@ public class SelfAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuc
 
     @Autowired
     private SecurityProperties securityProperties;
-
     @Autowired
     private ObjectMapper objectMapper;
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+    @Autowired
+    private AuthorizationServerTokenServices authenticationServerTokenServices;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response
@@ -37,12 +49,65 @@ public class SelfAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuc
 //        response.getWriter().write(objectMapper.writeValueAsString(authentication));//将authentication作为json写到前端
 
         log.info("自定义登录成功的处理器");
-        if (LoginType.JSON.equals(securityProperties.getBrowser().getLoginType())) {//如果配置的登录方式是json返回
-            response.setContentType("application/json;charset=utf-8");
-            response.getWriter().write(objectMapper.writeValueAsString(authentication));//将authentication作为json写到前端
-        }else{//如果不是json的登录返回方式，则调用父类的方式进行跳转（这也是为啥要继承SimpleUrlAuthenticationSuccessHandler的原因）
-            super.onAuthenticationSuccess(request,response,authentication);
+
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Basic ")) {
+//            chain.doFilter(request, response);
+//            return;
+            throw new UnapprovedClientAuthenticationException("请求头中没有client相关的信息");
         }
 
+        String[] tokens = extractAndDecodeHeader(header, request);
+        assert tokens.length == 2;
+        String clientId = tokens[0];
+        String clientSecret = tokens[1];
+        ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);//得到clientDetails信息
+
+        if (null == clientDetails) {
+            throw new UnapprovedClientAuthenticationException("clientid对应的信息不存在" + clientId);
+        } else if (!StringUtils.equals(clientSecret, clientDetails.getClientSecret())) {
+            throw new UnapprovedClientAuthenticationException("clientSecret信息不匹配" + clientSecret);
+        }
+
+        //构建自己的tokenRequest
+        TokenRequest tokenRequest = new TokenRequest(MapUtils.EMPTY_MAP, clientId, clientDetails.getScope(), "customer");
+
+        OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+
+        OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(oAuth2Request, authentication);
+
+        OAuth2AccessToken accessToken = authenticationServerTokenServices.createAccessToken(oAuth2Authentication);
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(objectMapper.writeValueAsString(accessToken));//将authentication作为json写到前端
+
+    }
+
+    /**
+     * Decodes the header into a username and password.
+     *
+     * @throws BadCredentialsException if the Basic header is not present or is not valid
+     *                                 Base64
+     */
+    private String[] extractAndDecodeHeader(String header, HttpServletRequest request)
+            throws IOException {
+
+        byte[] base64Token = header.substring(6).getBytes("UTF-8");
+        byte[] decoded;
+        try {
+            decoded = Base64.decode(base64Token);
+        } catch (IllegalArgumentException e) {
+            throw new BadCredentialsException(
+                    "Failed to decode basic authentication token");
+        }
+
+        String token = new String(decoded, "UTF-8");
+
+        int delim = token.indexOf(":");
+
+        if (delim == -1) {
+            throw new BadCredentialsException("Invalid basic authentication token");
+        }
+        return new String[]{token.substring(0, delim), token.substring(delim + 1)};
     }
 }
--- spring-security-app/src/main/java/com/learn/springsecurity/app/resourceconfig/ResourceServerConfig.java
+++ spring-security-app/src/main/java/com/learn/springsecurity/app/resourceconfig/ResourceServerConfig.java
@@ -1,7 +1,15 @@
 package com.learn.springsecurity.app.resourceconfig;
 
+import com.learn.springsecurity.app.authenticationhandler.SelfAuthenticationFailureHandler;
+import com.learn.springsecurity.app.authenticationhandler.SelfAuthenticationSuccessHandler;
+import com.learn.springsecurity.core.verify.ImageVerifyCodeFilter;
+import com.learn.springsecurity.core.verify.SmsVerifyCodeFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * autor:liman
@@ -10,5 +18,43 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  */
 @Configuration
 @EnableResourceServer
-public class ResourceServerConfig {
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    //自定义登录成功处理器
+    @Autowired
+    private SelfAuthenticationSuccessHandler selfAuthenticationSuccessHandler;
+    @Autowired
+    private SelfAuthenticationFailureHandler selfAuthenticationFailureHandler;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+//        ImageVerifyCodeFilter imageVerifyCodeFilter = new ImageVerifyCodeFilter();
+//        imageVerifyCodeFilter.setAuthenticationFailureHandler(selfAuthenticationFailureHandler);
+//        imageVerifyCodeFilter.setSecurityProperties(securityProperties);
+//        imageVerifyCodeFilter.afterPropertiesSet();
+//
+//        SmsVerifyCodeFilter smsVerifyCodeFilter = new SmsVerifyCodeFilter();
+//        smsVerifyCodeFilter.setAuthenticationFailureHandler(selfAuthenticationFailureHandler);
+//        smsVerifyCodeFilter.setSecurityProperties(securityProperties);
+//        smsVerifyCodeFilter.afterPropertiesSet();
+
+
+
+        http
+//                .addFilterBefore(imageVerifyCodeFilter, UsernamePasswordAuthenticationFilter.class)
+//                .addFilterBefore(smsVerifyCodeFilter, UsernamePasswordAuthenticationFilter.class)
+                .formLogin()//采用表单登录
+                .loginPage("/authentication/require")//指定登录的页面
+                .loginProcessingUrl("/authentication/form")//覆盖 UsernamePasswordAuthenticationFilter 中的请求配置，但最终处理这个请求的还是 UsernamePasswordAuthenticationFilter
+                .successHandler(selfAuthenticationSuccessHandler)//自定义登录成功处理器
+                .failureHandler(selfAuthenticationFailureHandler)
+                .and()
+                .authorizeRequests()//并且要认证请求
+                .antMatchers("/authentication/require",
+                        "/authentication/sessiontimeout",//session失效的路径放开登录校验
+                        "/verifycode/*").permitAll()//登录页的请求不需要认证
+                .anyRequest()//对任意的请求
+                .authenticated()//都需要做认证
+                .and().csrf().disable();//关闭csrf
+    }
 }
