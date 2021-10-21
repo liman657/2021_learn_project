49b8b2d3532f27c6474d3c1876f5612bbde7f10d
liman657 <657271181@qq.com>
2021-10-21 20:25:54 +0800

    【Java多线程】线程异常处理

---
+++ concurrency-foundation/src/main/java/com/learn/concurrency/foundataion/others/ListenerPatternDemo.java
@@ -0,0 +1,9 @@
+package com.learn.concurrency.foundataion.others;
+
+/**
+ * autor:liman
+ * createtime:2021-10-20
+ * comment:监听器设计模式
+ */
+public class ListenerPatternDemo {
+}
--- concurrency-foundation/src/main/java/com/learn/concurrency/foundataion/threadsecurity/ObjectPublishListenerThreadFix.java
+++ concurrency-foundation/src/main/java/com/learn/concurrency/foundataion/threadsecurity/ObjectPublishListenerThreadFix.java
@@ -3,20 +3,27 @@ package com.learn.concurrency.foundataion.threadsecurity;
 /**
  * autor:liman
  * createtime:2021/9/27
- * comment: 比较隐晦的，监听器的时候，对象未初始化完成
+ * comment: 监听器的异常修复，用工厂模式修复
  */
-public class ObjectPublishListener {
+public class ObjectPublishListenerThreadFix {
 
     int count;
+    private EventListener eventListener;
 
-    public ObjectPublishListener(MySource mySource) {
-        mySource.registerListener(new EventListener() {
-            //这里需要访问外部的count，但是这里过早的暴露了EventListener对象，过早的操作没有初始化完成的count，会产生相关问题。
+    public static ObjectPublishListenerThreadFix getInstance(MySource mySource){
+        //初始化完成之后，再注册监听器
+        ObjectPublishListenerThreadFix listenerThreadFix = new ObjectPublishListenerThreadFix(mySource);
+        mySource.registerListener(listenerThreadFix.eventListener);
+        return listenerThreadFix;
+    }
+
+    private ObjectPublishListenerThreadFix(MySource mySource) {
+        eventListener = new EventListener() {
             @Override
             public void onEvent(Event e) {
                 System.out.print("我得到的数字是" + count);
             }
-        });
+        };
 
         for (int i = 0; i < 1000; i++) {
             System.out.print(i);
@@ -40,7 +47,7 @@ public class ObjectPublishListener {
         }).start();
 
         //这里调用构造函数，构造函数里头会完成监听器的注册，
-        ObjectPublishListener objectPublishListener = new ObjectPublishListener(source);
+        ObjectPublishListenerThreadFix objectPublishListener = new ObjectPublishListenerThreadFix(source);
     }
 
     /**
