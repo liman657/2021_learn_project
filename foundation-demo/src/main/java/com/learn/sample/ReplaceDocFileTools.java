f7238c5c3c2ab0ae60c1b095b9e1013a2fadd38d
liman657 <657271181@qq.com>
2021-11-07 13:53:49 +0800

    markdown文档批量替换图片路径工具

--- foundation-demo/src/main/java/com/learn/sample/ReplaceDocFileTools.java
+++ foundation-demo/src/main/java/com/learn/sample/ReplaceDocFileTools.java
@@ -29,7 +29,7 @@ public class ReplaceDocFileTools {
     }
 
     public static void main(String[] args) throws IOException {
-
+        long startTime = System.currentTimeMillis();
         String path = "F:\\blog_doc\\博客文档";
         //1.读取文件夹下的所有视频文件
         List<File> fileList = new ArrayList<>();
@@ -41,7 +41,9 @@ public class ReplaceDocFileTools {
         String toRegexStr = "E{1}\\:\\\\blogPic\\b";
         String toReplaceStr = "F:\\\\blog_doc\\\\blogPic";
         fileList.stream().forEach(t -> replaceImgPath(t, toRegexStr, toReplaceStr));
-        log.info("批量文件字符串替换完成，原始文件个数:{}，总共替换字符串:{}个",fileNameList.size(),count);
+        long endTime = System.currentTimeMillis();
+        long costTime = endTime - startTime;
+        log.info("批量文件字符串替换完成，原始文件个数:{}，总共替换字符串:{}个，总共耗时:{}ms",fileNameList.size(),count,costTime);
     }
 
     /**
@@ -82,23 +84,27 @@ public class ReplaceDocFileTools {
 
     /**
      * 开始替换文件中的内容
-     *
-     * @param sourceFile
+     * @param sourceFile 源文件
+     * @param targetRegex 需要匹配的正则表达式
+     * @param toReplaceStr 需要替换成的字符串
      */
     public static void replaceImgPath(File sourceFile, String targetRegex, String toReplaceStr) {
         String regex = targetRegex;
-        String targetFileDir = "F:\\博客文档_新";
+        String targetFileDir = "F:\\博客文档_新";//目标文件夹，处理之后的文件，都会写入到这个文件夹中，并且保证按照原有文件路径分类
         BufferedReader reader = null;
         BufferedWriter writer = null;
         log.info("开始处理文件:{},最终文件会存在目录:{}中",sourceFile.getAbsoluteFile(),targetFileDir);
         String fileName = sourceFile.getAbsoluteFile() + "/" + sourceFile.getName();
         try {
+            //构造源文件读取器
             reader = new BufferedReader(new InputStreamReader(new FileInputStream(sourceFile)));
+            //创建目标文件
             File targetFile = createTargetFile(sourceFile,targetFileDir);
             if(null == targetFile){
                 log.info("文件:{},目标文件创建失败，请手动操作",sourceFile.getAbsolutePath());
                 return;
             }
+            //构造目标文件写入器
             writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(targetFile)));
             String tempStr = "";
             String regStr = regex;
@@ -106,7 +112,8 @@ public class ReplaceDocFileTools {
             while ((tempStr = reader.readLine()) != null) {
                 tempStr += "\n"; //加上换行，避免格式错乱
                 Matcher matcher = pattern.matcher(tempStr);
-                if (matcher.find()) {//如果匹配上
+                if (matcher.find()) {//如果匹配上，进行替换
+                    //按照指定正则替换
                     tempStr = tempStr.replaceAll(regex, toReplaceStr);
                     log.info("文件:{},替换后的字符串为:{}",sourceFile.getAbsoluteFile(), tempStr);
                     count++;
@@ -115,8 +122,11 @@ public class ReplaceDocFileTools {
             }
             writer.flush();
         } catch (Exception e) {
-            log.error("文件:{},字符替换异常，请手动操作", fileName);
+            //记录异常
+            log.error("文件:{},字符替换异常，异常信息为:{}，请手动操作", fileName,e);
+            return;
         } finally {
+            //关闭流
             try {
                 reader.close();
             } catch (IOException e) {
@@ -130,18 +140,30 @@ public class ReplaceDocFileTools {
         }
     }
 
+    /**
+     * 根据原文件路径，创建目标文件
+     * @param sourceFile 源文件
+     * @param targetFileDir 目标文件根路径
+     * @return
+     */
     public static File createTargetFile(File sourceFile,String targetFileDir){
+        //获取源文件绝对路径
         String sourceFileAbsoluteName = sourceFile.getAbsolutePath();
         String afterDealFileName = sourceFileAbsoluteName.replace("F:\\blog_doc", targetFileDir);
+        //截取出文件夹路径和文件名
         int splitIndex = afterDealFileName.lastIndexOf("\\");
+        //文件夹路径
         String dirPath = afterDealFileName.substring(0,splitIndex+1);
+        //文件名
         String createFileName = afterDealFileName.substring(splitIndex+1);
         log.info("准备创建的目标文件名为:{},存在于:{}文件夹",createFileName,dirPath);
+        //先递归创建文件夹
         File dirFile = new File(dirPath);
         if(!dirFile.exists()){
             dirFile.mkdirs();
             log.info("文件夹:{}创建完成",dirPath);
         }
+        //创建目标文件
         File targetFile = new File(dirPath,createFileName);
         if(!targetFile.exists()){
             try {
@@ -152,8 +174,7 @@ public class ReplaceDocFileTools {
             }
             log.info("文件:{}创建完成",targetFile.getAbsolutePath());
         }
+        //返回已经创建的文件
         return targetFile;
-
-
     }
 }
